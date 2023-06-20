9164b99c97e530f9bfd982fece071dfa3e354962
avive <aviveyal07@gmail.com>
2023-06-20 13:32:36 +0300

    Build types

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,8 +1,3 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-export * from './chain/types.js';
-export * from './events/types.js';
-export * from './identity/types.js';
-export * from './transactions/types.js';
-export * from './verifier/types.js';
